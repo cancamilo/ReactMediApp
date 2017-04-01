942168afd240419d7c537f7c109201aa71243674
camiloramirezf@hotmail.com <camiloramirezf@hotmail.com>
2017-04-02 01:51:25 +0200

    full app again

--- app/app.jsx
+++ app/app.jsx
@@ -3,26 +3,26 @@ var ReactDOM = require('react-dom');
 
 // Allows children element to dispatch actionss
 var {Provider} = require('react-redux');
-//var {hashHistory} = require('react-router');
-//import firebase, {mediplansApp} from 'app/firebase/firebaseConfig';
-//import router from 'app/router/';
-//var actions = require('actions');
+var {hashHistory} = require('react-router');
+import firebase, {mediplansApp} from 'app/firebase/firebaseConfig';
+import router from 'app/router/';
+var actions = require('actions');
 
 var store = require('configureStore').configure();
 
-// mediplansApp.auth().onAuthStateChanged((user) =>{
-//   if(user) {
-//     console.log("on auth state changed...logged in");
-//     store.dispatch(actions.loginUser(user));
-//     store.dispatch(actions.clearTables());
-//     store.dispatch(actions.startAddTables());
-//     hashHistory.push('/DashboardApp/MediPlansList');
-//   } else {
-//     console.log("on auth state changed...no user");
-//     store.dispatch(actions.logoutUser());
-//     hashHistory.push('/');
-//   }
-// });
+mediplansApp.auth().onAuthStateChanged((user) =>{
+  if(user) {
+    console.log("on auth state changed...logged in");
+    store.dispatch(actions.loginUser(user));
+    store.dispatch(actions.clearTables());
+    store.dispatch(actions.startAddTables());
+    hashHistory.push('/DashboardApp/MediPlansList');
+  } else {
+    console.log("on auth state changed...no user");
+    store.dispatch(actions.logoutUser());
+    hashHistory.push('/');
+  }
+});
 
 // Load foundation
 $(document).foundation();
@@ -33,7 +33,7 @@ require('style!css!sass!applicationStyles');
 ReactDOM.render(
   // TodoApp component and all of its children will be able to access data in the store and dispatcha actions
   <Provider store = {store}>
-    <h1>Testing!</h1>
+    {router}
   </Provider>,
   document.getElementById('app')
 );
